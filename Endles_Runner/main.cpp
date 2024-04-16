654221185fc9e688dfd77ba11fb212ae0c5afec0
nachosou <nachosou123@gmail.com>
2024-04-16 16:31:59 -0300

    Add functional window

--- Endles_Runner/main.cpp
+++ Endles_Runner/main.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	sf::RenderWindow window(sf::VideoMode(1366, 768), "SFML window");
+
+	sf::RectangleShape player;
+
+	player.setPosition({200, 200});
+	player.setFillColor(sf::Color::Red);
+	player.setSize({40, 40});
+
+	while (window.isOpen())
+	{
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
 
 	return 0;
 }
